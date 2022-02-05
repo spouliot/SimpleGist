21393275c60eaf08e32f0df2f638c63417ada3a9
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2022-02-05 16:49:50 -0500

    Initial version

---
+++ Program.cs
@@ -0,0 +1,52 @@
+// MIT License
+//
+// Copyright (c) 2022 Sebastien Pouliot
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using SimpleGist;
+
+if (args.Length == 0) {
+	Console.WriteLine ("Usage: SimpleGist <file> [<file2> ...]");
+	return 1;
+}
+
+var token = Environment.GetEnvironmentVariable ("GITHUB_OAUTH_TOKEN");
+if (token is null) {
+	var home = Environment.GetEnvironmentVariable ("HOME");
+	if (home is not null)
+		token = File.ReadAllText (Path.Combine (home, ".gist"));
+}
+GistClient.OAuthToken = token;
+
+GistRequest request = new () {
+	Description = "My private gist",
+	Public = false,
+};
+foreach (var arg in args) {
+	if (!File.Exists (arg)) {
+		Console.WriteLine ($"File {arg} does not exist");
+		return 1;
+	}
+	request.AddFile (arg, File.ReadAllText (arg));
+}
+
+var response = await GistClient.CreateAsync (request);
+Console.WriteLine (response.Url);
+return 0;
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SimpleGist
 A simple minimalistic gist client for C#
+
+Nothing wrong with [Octokit.net](https://github.com/octokit/octokit.net)
+but it's a bit too big for my taste and my tools only needs so little of
+it.
+
+This was made in order to minimize dependencies and make it linker 
+friendly.
+
+* `Program.cs` is the test application
+* `SimpleGist.cs` is the gist client that you can copy into your projects
